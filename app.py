343afd973322ad2f454bf16f5dae8b3e57385b3d
ZaraZa9 <150523620+ZaraZa9@users.noreply.github.com>
2023-11-11 21:52:06 +0000

    commit

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from pathlib import Path
 import base64
-
+import requests
+import json
 # Initial page config
 st.set_page_config(
      page_title='Streamlit cheat sheet',
@@ -10,28 +11,37 @@ st.set_page_config(
 )
 
 def main():
-    cs_sidebar()
 
-    #Sidebar inputs are sent to dataset
-
+    allSubreddits = ["Donotmove", "yesplease"]
+    cs_sidebar(allSubreddits) 
+    
     cs_body()
     return None
 
 
-def cs_sidebar():
+def cs_sidebar(subredditList):
 
     sizeSample = st.sidebar.slider("Select sample size: ", min_value=1, max_value=100, value=1)
-    seed_input = st.sidebar.text_area("Enter Seed: ", "0")
+    imageOutput = st.sidebar.checkbox("Would you like an image output ")
+    selectedReddits = st.sidebar.multiselect("Select your subreddits:", subredditList)
+
+    seedInput = st.sidebar.text_area("Enter Seed: ", "0")
     try:
-        int(seed_input)
-        if len(str(seed_input)) != 10:
+        int(seedInput)
+        if len(str(seedInput)) != 10:
             st.sidebar.text("Seed must be 10 digits")
-        else:
-            st.sidebar.text("Success")
     except:
         st.sidebar.text("Error: Only accept integers")
 
-    return sizeSample, seed_input
+    button_clicked2 = st.sidebar.button("Generate GPT response")
+    if button_clicked2:
+        postData(sizeSample, imageOutput, selectedReddits, seedInput)
+
+def postData(sizeSample, imageOutput, selectedReddits, seedInput):
+    url = False
+    data = {"size-sample": sizeSample, "image-output": imageOutput, "selected-reddits": selectedReddits, "seed-inputs": seedInput}
+
+    response = requests.post(url, json.dumps(data))
 
 
 ##########################
@@ -51,26 +61,33 @@ def display_image_from_sdiffusion(url):
     return None
 
 
-def generate(): #function to generate 
-    st.write("Button clicked! My custom function executed.")
+def get_image(): #function to generate 
+    response = requests.get("https://api.example.com/data")
+    data = response.json()
+    return data["URL"] #returns image URL
+
+def get_graph_data():
+    #return (json.load(requests.get("https://api.example.com/data")))["Array1"] , (json.load(requests.get("https://api.example.com/data")))["Array2"]
+    return [1,2,3,4,5,6] , [12,10,14,9,7,21] 
+
+def get_GPT_data():
+    #return (json.load(requests.get("https://api.example.com/data")).json)["GPT_text"]
+    return "Hello World"
 
 
 
-def cs_body():
-    text_message = "Hello, this is a text message." # example data
-    data = {
-    'Variable1': [1, 2, 3, 4, 5],
-    'Variable2': [10, 15, 8, 12, 7] #example data
+def cs_body(inputList):
+    
+
+
+    GPT_text = get_GPT_data()
+    graph_data = {
+    'Variable1': get_graph_data()[0],
+    'Variable2': get_graph_data()[1] #example data
     }
-    display_text_in_textbox(text_message)
-    # Create a button
-    button_clicked = st.button("Generate a retard")
-
-# Check if the button is clicked
-    if button_clicked:
-        generate()
-    st.bar_chart(data)
-    display_image_from_sdiffusion("https://example.com/your_image.jpg")
+    st.bar_chart(graph_data)
+    display_text_in_textbox(GPT_text)    
+    #display_image_from_sdiffusion(url) #<---------- image url from side menu here
     return None
 
 # Run main()
